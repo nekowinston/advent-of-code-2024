df94b71f7982015b006556eaeada7f47fc00e62f
winston <hey@winston.sh>
2024-12-02 14:02:38 +0100

    chore: move some stuff for day 2

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,19 @@
 module Main (main) where
 
 import qualified AOC.Day1
+import qualified AOC.Day2
+
+printSolutions :: (Show a1, Show a2) => (p -> a1) -> (p -> a2) -> p -> IO ()
+printSolutions s1 s2 input =
+  putStr $
+    unlines $
+      map unwords [["Part 1:", show solution1], ["Part 2:", show solution2]]
+ where
+  solution1 = s1 input
+  solution2 = s2 input
 
 main :: IO ()
-main = do
-  content <- readFile "data/day1/input.txt"
-  AOC.Day1.printSolutions content
+main = readFile "data/day1.txt"
+  >>= printSolutions AOC.Day1.solve1 AOC.Day1.solve2
+  >> readFile "data/day2.txt"
+  >>= printSolutions AOC.Day2.solve1 AOC.Day2.solve2
--- src/AOC/Day1.hs
+++ src/AOC/Day1.hs
@@ -1,4 +1,4 @@
-module AOC.Day1 (printSolutions, solve1, solve2) where
+module AOC.Day1 (solve1, solve2) where
 
 import Data.Bifunctor (bimap)
 import Data.List (sort)
@@ -13,29 +13,17 @@ count :: (Eq a) => a -> [a] -> Int
 count x = length . filter (x ==)
 
 parseInput :: String -> [(Int, Int)]
-parseInput content = do
-  map (tuplify . map read . words) $ lines content
+parseInput content = map (tuplify . map read . words) $ lines content
 
 solve1 :: String -> Int
-solve1 input = do
-  sum listOfDiffs
+solve1 input = sum listOfDiffs
  where
   parsed = parseInput input
   sortedListOfTuples = uncurry zip $ bimap sort sort $ unzip parsed
   listOfDiffs = map (abs . uncurry (-)) sortedListOfTuples
 
 solve2 :: String -> Int
-solve2 input = do
-  sum listOfSimilarities
+solve2 input = sum listOfSimilarities
  where
   (left, right) = unzip $ parseInput input
   listOfSimilarities = map (\x -> x * count x right) left
-
-printSolutions :: String -> IO ()
-printSolutions input =
-  putStr $
-    unlines $
-      map unwords [["Part 1:", show solution1], ["Part 2:", show solution2]]
- where
-  solution1 = solve1 input
-  solution2 = solve2 input
