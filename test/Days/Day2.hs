b0cab27d411146c06aad8a6a9490b58069459c76
winston <hey@winston.sh>
2024-12-02 16:48:15 +0100

    style: reformat

--- test/Days/Day2.hs
+++ test/Days/Day2.hs
@@ -6,21 +6,21 @@ import AOC.Day2
 
 sample :: String
 sample =
-    unlines
-        [ "7 6 4 2 1"
-        , "1 2 7 8 9"
-        , "9 7 6 2 1"
-        , "1 3 2 4 5"
-        , "8 6 4 4 1"
-        , "1 3 6 7 9"
-        ]
+  unlines
+    [ "7 6 4 2 1"
+    , "1 2 7 8 9"
+    , "9 7 6 2 1"
+    , "1 3 2 4 5"
+    , "8 6 4 4 1"
+    , "1 3 6 7 9"
+    ]
 
 test1 :: Test
 test1 =
-    TestCase $
-        assertEqual "reports are correctly classified as safe/unsafe" (solve1 sample) 2
+  TestCase $
+    assertEqual "reports are correctly classified as safe/unsafe" (solve1 sample) 2
 
 test2 :: Test
 test2 =
-    TestCase $
-        assertEqual "report classification respects the error dampener" (solve2 sample) 4
+  TestCase $
+    assertEqual "report classification respects the error dampener" (solve2 sample) 4
--- test/Spec.hs
+++ test/Spec.hs
@@ -6,16 +6,16 @@ import qualified Days.Day2
 
 tests :: Test
 tests =
-    TestList
-        [ TestLabel "day 1, part 1" Days.Day1.test1
-        , TestLabel "day 1, part 2" Days.Day1.test2
-        , TestLabel "day 2, part 1" Days.Day2.test1
-        , TestLabel "day 2, part 2" Days.Day2.test2
-        ]
+  TestList
+    [ TestLabel "day 1, part 1" Days.Day1.test1
+    , TestLabel "day 1, part 2" Days.Day1.test2
+    , TestLabel "day 2, part 1" Days.Day2.test1
+    , TestLabel "day 2, part 2" Days.Day2.test2
+    ]
 
 main :: IO ()
 main = do
-    counts' <- runTestTT tests
-    if (errors counts' + failures counts' == 0)
-        then exitSuccess
-        else exitFailure
+  counts' <- runTestTT tests
+  if (errors counts' + failures counts' == 0)
+    then exitSuccess
+    else exitFailure
