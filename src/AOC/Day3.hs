bd95efeee911d6956e1ccea9bfd7829ef9c71eec
winston <hey@winston.sh>
2024-12-03 17:13:29 +0100

    feat: day 3, part 2

--- src/AOC/Day3.hs
+++ src/AOC/Day3.hs
@@ -3,25 +3,50 @@
 
 module AOC.Day3 where
 
+import Data.Maybe (listToMaybe)
 import Text.RawString.QQ (r)
 import Text.Regex.TDFA
-
-result :: Integer
-result = 161
 
 type Instruction = (Int, Int)
 
 mulRegex :: String
 mulRegex = [r|mul\(([[:digit:]]+),([[:digit:]]+)\)|]
 
-parseInstructions :: String -> [Instruction] -> [Instruction]
-parseInstructions input acc =
+parseInstructions1 :: String -> [Instruction] -> [Instruction]
+parseInstructions1 input acc =
   case input =~ mulRegex :: (String, String, String, [String]) of
-    (_, _, rest, [x, y]) -> parseInstructions rest ((read x, read y) : acc)
+    (_, _, rest, [x, y]) -> parseInstructions1 rest ((read x, read y) : acc)
     _ -> reverse acc
 
 solve1 :: String -> Int
-solve1 = sum . uncurry (zipWith (*)) . unzip . flip parseInstructions []
+solve1 = sum . uncurry (zipWith (*)) . unzip . flip parseInstructions1 []
+
+toggleRegex :: String
+toggleRegex = [r|do(n't)?\(\)|]
+
+data ParseState = ToggleOn | ToggleOff | Keep
+
+checkForToggleInstruction :: String -> ParseState
+checkForToggleInstruction input =
+  case listToMaybe instructions of
+    Just "don't()" -> ToggleOff
+    Just "do()" -> ToggleOn
+    _ -> Keep
+ where
+  instructions = reverse $ getAllTextMatches (input =~ toggleRegex) :: [String]
+
+parseInstructions2 :: String -> [Instruction] -> Bool -> [Instruction]
+parseInstructions2 input acc enabled = do
+  case input =~ mulRegex :: (String, String, String, [String]) of
+    (prefix, _, rest, [x, y]) -> do
+      let
+        shouldAct = case (checkForToggleInstruction prefix, enabled) of
+          (ToggleOn, _) -> True
+          (ToggleOff, _) -> False
+          (_, keep) -> keep
+        (xInt, yInt) = if shouldAct then (read x, read y) else (0, 0)
+      parseInstructions2 rest ((xInt, yInt) : acc) shouldAct
+    _ -> reverse acc
 
 solve2 :: String -> Int
-solve2 input = 0
+solve2 input = sum . uncurry (zipWith (*)) . unzip $ parseInstructions2 input [] True
--- test/Days/Day3.hs
+++ test/Days/Day3.hs
@@ -4,15 +4,18 @@ import Test.HUnit
 
 import AOC.Day3
 
-sample :: String
-sample = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+sample1 :: String
+sample1 = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 
 test1 :: Test
 test1 =
   TestCase $
-    assertEqual "mul instructions work correctly" (solve1 sample) 161
+    assertEqual "mul instructions work correctly" (solve1 sample1) 161
+
+sample2 :: String
+sample2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
 test2 :: Test
 test2 =
   TestCase $
-    assertEqual "TODO" (solve2 sample) 0
+    assertEqual "TODO" (solve2 sample2) 48
