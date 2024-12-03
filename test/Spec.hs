9fe64f85aa9e48bf86c0ef65baf22ad275b37adf
winston <hey@winston.sh>
2024-12-03 17:43:13 +0100

    test: add day 3 to test suite

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,6 +3,7 @@ import Test.HUnit
 
 import qualified Days.Day1
 import qualified Days.Day2
+import qualified Days.Day3
 
 tests :: Test
 tests =
@@ -11,11 +12,13 @@ tests =
     , TestLabel "day 1, part 2" Days.Day1.test2
     , TestLabel "day 2, part 1" Days.Day2.test1
     , TestLabel "day 2, part 2" Days.Day2.test2
+    , TestLabel "day 3, part 1" Days.Day3.test1
+    , TestLabel "day 3, part 2" Days.Day3.test2
     ]
 
 main :: IO ()
 main = do
   counts' <- runTestTT tests
-  if (errors counts' + failures counts' == 0)
+  if errors counts' + failures counts' == 0
     then exitSuccess
     else exitFailure
