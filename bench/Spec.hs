fd631044516234f0f099e8ef43ee10914bd6aece
winston <hey@winston.sh>
2024-12-05 05:19:15 +0100

    test: fix inputs for benchmark

--- bench/Spec.hs
+++ bench/Spec.hs
@@ -27,7 +27,7 @@ main =
   solvers :: [(Solver, Solver, String)]
   solvers =
     [ (AOC.Day1.solve1, AOC.Day1.solve2, $(embedStringFile "./data/day1.txt"))
-    , (AOC.Day2.solve1, AOC.Day2.solve2, $(embedStringFile "./data/day1.txt"))
-    , (AOC.Day3.solve1, AOC.Day3.solve2, $(embedStringFile "./data/day1.txt"))
-    , (AOC.Day4.solve1, AOC.Day4.solve2, $(embedStringFile "./data/day1.txt"))
+    , (AOC.Day2.solve1, AOC.Day2.solve2, $(embedStringFile "./data/day2.txt"))
+    , (AOC.Day3.solve1, AOC.Day3.solve2, $(embedStringFile "./data/day3.txt"))
+    , (AOC.Day4.solve1, AOC.Day4.solve2, $(embedStringFile "./data/day4.txt"))
     ]
